0ab8ff370982a9d9fece6acbadb9262eee1e70ab
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-04-15 18:15:56 +0900

    Add name column to users table
    Added name column with below two constraints by terminal command “rails
    g migration AddNameToUsers name:string” and “rake db:migrate”
    
    *NOT NULL constraint
    *unique constraint with index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415023552) do
+ActiveRecord::Schema.define(version: 20170415071219) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -25,7 +25,9 @@ ActiveRecord::Schema.define(version: 20170415023552) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name",                                null: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["name"], name: "index_users_on_name", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
