6fc8106a75a8107619b65f64b91341376882a0f8
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-04-19 15:25:12 +0900

    fixed some syntax

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,8 +8,8 @@ protected
 #update_without_current_password：models/user.rbに自分で定義したメソッド
 #現在のパスワードの入力無しでもパスワードの更新をできるようにした
 
-  def update_resource(resource, params)
-    resource.update_without_current_password(params)
-  end
+def update_resource(resource, params)
+  resource.update_without_current_password(params)
+end
 
 end
