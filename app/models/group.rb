3ad9ad21845c2b6dc5db0ccf8806425d0d7b2600
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-04-22 20:25:28 +0900

    defined accepts_nested_attributes_for in group model
    To save records in groups table and joins table(middle table of groups
    and users) at the same time.

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,7 @@ class Group < ApplicationRecord
   has_many :joins
   has_many :users, through: :joins
 
+  # グループを新規作成・更新したときに、アソシエーション先であるusersも含めて保存・更新できるようにする
+  accepts_nested_attributes_for :users
+
 end
