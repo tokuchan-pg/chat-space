9ce3665815af8c288228add9661f0fb4af41bebc
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-04-22 20:28:21 +0900

    defined new & create action in groups_controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,18 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+  end
+
+  def create
+    Group.create(group_params)
+    redirect_to root_path
+  end
+
+  private
+  def group_params
+    # :user_idsは配列なので、書き方が↓のように特殊な形となる
+    params.require(:group).permit(:name, :user_ids => [])
   end
 
 end
