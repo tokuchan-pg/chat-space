dea48485663b7edc26b88fd666351f63170dbf7f
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-04-24 21:32:02 +0900

    defined group#edit & group#update action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -15,6 +15,21 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      flash.notice = 'グループが更新されました'
+      redirect_to root_path
+    else
+      flash.now.alert = 'グループ名を入力して下さい'
+      render :edit
+    end
+  end
+
   private
   def group_params
     # ログインユーザーのidを、collection_check_boxes経由で送られてきた配列user_idsに、文字列型で追加する
