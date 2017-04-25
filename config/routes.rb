e40ca308cab36b46498c0efa9bdbfe8d25c43de9
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-04-25 17:07:46 +0900

    defined routing for messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :groups, except: [:show, :destroy]
+  resources :groups, except: [:show, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
 
   root 'messages#index'
 end
