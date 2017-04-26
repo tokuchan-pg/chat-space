8a76cc30e63eefcd7e845747db034a252b8e7c81
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-04-26 13:12:12 +0900

    used association to set instance variable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
   end
 
   def create
-    @message = Message.new(message_params)
+    @message = current_user.messages.new(message_params)
     if @message.save
       redirect_to group_messages_path(@group), notice: '新規メッセージが送信されました'
     else
