26d2b2fc1af9cc30baedfd17c366a6e0294e45b9
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-04-26 11:37:13 +0900

    generated messages_controller and defined index & create action
    At the same time, defined validation in message model. Create action
    shows appropriate flash message after a message saved or not.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,33 @@
 class MessagesController < ApplicationController
+
+  before_action :set_group
+  before_action :set_messages
+
   def index
+    @message = Message.new
   end
+
+  def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: '新規メッセージが送信されました'
+    else
+      flash.now.alert = 'メッセージを入力して下さい'
+      render :index
+    end
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
+  def set_messages
+    @messages = Message.where(group_id: params[:group_id])
+  end
+
 end
