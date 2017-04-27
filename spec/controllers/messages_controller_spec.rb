bf860d59e208c33408b1f375e27631053f8e994a
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-04-27 22:10:41 +0900

    added more controller tests
    Added below 3 patterns
    
    *GET #index & POST #create
    whether it assigns the requested group to @group.
    
    *GET #index & POST #create
    whether it assigns the requested messages to @messages.
    
    *POST #create
    whether it renders to messages#index if creating a message failed

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -12,6 +12,17 @@ describe MessagesController do
 
     describe 'GET #index' do
 
+      it "assigns the requested group to @group" do
+        get :index, params:{ group_id: group.id }
+        expect(assigns(:group)).to eq group
+      end
+
+      it "assigns the requested messages to @messages" do
+        messages = create_list(:message, 3 , group_id: group.id)
+        get :index, params:{ group_id: messages.first.group_id }
+        expect(assigns(:messages)).to match messages
+      end
+
       it "assigns the new message to @message" do
         get :index, params:{ group_id: group.id }
         #ActualがMessageクラスのインスタンスであるかどうかをテスト
@@ -27,6 +38,17 @@ describe MessagesController do
 
     describe 'POST #create' do
 
+      it "assigns the requested group to @group" do
+        get :index, params:{ group_id: group.id }
+        expect(assigns(:group)).to eq group
+      end
+
+      it "assigns the requested messages to @messages" do
+        messages = create_list(:message, 3 , group_id: group.id)
+        get :index, params:{ group_id: messages.first.group_id }
+        expect(assigns(:messages)).to match messages
+      end
+
       it "assigns the new message to @message with strong parameter" do
         post :create, params:{ group_id: group.id, message: attributes_for(:message)}
         expect(assigns(:message).body).to eq message.body
@@ -35,9 +57,24 @@ describe MessagesController do
         expect(assigns(:message).user_id).to eq message.user_id
       end
 
-      it "redirects to messages#index" do
-        post :create, params:{ group_id: group.id, message: attributes_for(:message)}
-        expect(response).to redirect_to group_messages_path
+      context 'if creating a message succeeded' do
+
+        it "redirects to messages#index" do
+          post :create, params:{ group_id: group.id, message: attributes_for(:message)}
+          expect(response).to redirect_to group_messages_path
+        end
+
+      end
+
+      context 'if creating a message failed' do
+
+        it "renders to messages#index" do
+          #bodyがブランクのparamsを渡すことで失敗時の状況を作る
+          post :create, params:{ group_id: group.id, message: attributes_for(:message, body: "")}
+          #リダイレクトではなくrenderなので、プレフィックスでテンプレートを指定することはできない
+          expect(response).to render_template :index
+        end
+
       end
 
     end
