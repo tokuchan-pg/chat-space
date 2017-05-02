50bc381ba8db8ee56fc1e37f7b57f6cdf5346f29
tokuchan-pg <takahide.tokuchi@gmail.com>
2017-05-02 19:32:43 +0900

    changed the condition of searching

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where('name LIKE(?)', "#{user_params[:user]}%").where.not(id: current_user.id)
+    @users = User.where('name LIKE(?)', "%#{user_params[:user]}%").where.not(id: current_user.id)
     respond_to do |format|
       format.html { redirect_to new_group_path}
       format.json
